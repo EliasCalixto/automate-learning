8a5bb6f30cdaeeb5f02215b1db6f1d37972a62a1
Elias Calixto <eliascalixto989@gmail.com>
2021-09-27 21:33:29 -0500

    added automate and free

---
+++ automate/2.py
@@ -0,0 +1,10 @@
+# este es un comentario #
+while True: 
+	print("Who are you?")
+	me = input()
+	if me != 'Elias':
+		print("get out stranger")
+		continue
+	else:
+		print("welcome darkesthj")
+	break
---
+++ automate/spam.py
@@ -0,0 +1,10 @@
+spam = input()
+if spam == "1":
+    print("Hello")
+else:
+    if spam == "2":
+        print("Howdy")
+    else:
+        print("Greetings!")
+
+print("I'm the main")
