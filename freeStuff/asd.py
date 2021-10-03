bbbdd8831cbde58baa3c5c174682edf2d8234a7c
Elias Calixto <eliascalixto989@gmail.com>
2021-10-03 15:30:05 -0500

    03/01 update

--- freeStuff/asd.py
+++ freeStuff/asd.py
@@ -1,5 +1,10 @@
-def elias():
-    a = input('pon algo plox: ')
-    b = input('ingresa algo mas: ')
-        
+def calc(x,y):
+    z = x + y
+    return(z)
 
+a = calc(1,2)
+b = calc(4,2)
+c = calc(5,6)
+
+d = a + b + c 
+print(d)
