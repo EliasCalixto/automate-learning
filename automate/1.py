73ec110b261f0d3356d18bf36ca79dde80b9b880
Elias Calixto <eliascalixto989@gmail.com>
2021-10-10 04:05:17 -0500

    asd

--- automate/1.py
+++ automate/1.py
@@ -1,25 +1,8 @@
-while True:
-	print("What's your nickname?")
-	nickname = input()
-	if nickname != "darkesthj":
-		print("You are not el mejor de la region")
-		continue
-	else:
-		print("Now please give us your password")
-	password = input()
-	print("Please repeat your password")
-	password2 = input()
-	if password2 != password:
-		print("wrong password idiot")
-		continue
-	else:
-		print("hi darkesthj :D you are the best")
-	break
-while True:
-	print("Now what do you want to do "+ nickname +"?")
-	toDo = input()
-	if toDo != "I wanna fuck":
-		print("Are you sure bb?")
-	else:
-		print("I wanna fuck too :(")
+def elias():
+	return 'Hola amigos'
 
+def asd(x,y):
+	return x + y
+
+print(asd(34,65))
+	
