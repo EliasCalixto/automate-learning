6f44793af0c02e7f84002d44d29cbc0083a328f3
Elias Calixto <eliascalixto989@gmail.com>
2021-10-15 00:59:34 -0500

    ww

--- freeStuff/modules.py
+++ freeStuff/modules.py
@@ -1,7 +1,15 @@
 import random
 
-random.randint(25,78)
+def asd():   
+    a = random.randint(2,6)
+    b = random.randint(1,4)
+    c = a**b
+    print(c)
 
+    if c < 1000:
+        asd()
+
+asd()
 
 
 
