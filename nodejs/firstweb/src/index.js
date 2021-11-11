267b7fbd84424c5d8afcab4865f8f1314b3a93a0
darkesthj <85374767+EliasCalixto@users.noreply.github.com>
2021-11-10 21:19:48 -0500

    b

--- nodejs/firstweb/src/index.js
+++ nodejs/firstweb/src/index.js
@@ -6,9 +6,7 @@ app.set('port', 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-	res.render('index', { title: 'First Website' });
-});
+app.use(require('./routes/index'));
 
 app.listen(app.get('port'), () => {
 	console.log('Server on port', app.get('port'));
