4ca164a37a5ef4b1678fc2f3ea7d1d8cf0f3c5ed
darkesthj <85374767+EliasCalixto@users.noreply.github.com>
2021-12-04 20:07:37 -0500

    ss

---
+++ passingReference.py
@@ -0,0 +1,16 @@
+def eggs(someParameter):
+    return someParameter.append('Hello')
+
+spam = [1,2,3]
+print(spam)
+print('este es spam')
+asd = spam
+print(asd)
+print('este es asd')
+eggs(spam)
+print(spam)
+print('este es spam')
+print(asd)
+print('este es asd')
+
+
