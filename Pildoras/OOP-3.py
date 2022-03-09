b9b9c944ed12ca0a218509eb8f4faea3afa3f612
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2022-03-09 00:45:31 -0500

    d

--- Pildoras/OOP-3.py
+++ Pildoras/OOP-3.py
@@ -1,30 +1,46 @@
 class Coche():
     #propierties
-    largoChasis = 250
-    anchoChasis = 120
-    ruedas = 4
-    enMarcha = False
+    def __init__(self):
+        self.__largoChasis = 250
+        self.__anchoChasis = 120
+        self.__ruedas = 4
+        self.__enMarcha = False
     #behaviors
     def arrancar(self, arrancamos):
-        self.enMarcha = arrancamos
-        if (self.enMarcha):
+        self.__enMarcha = arrancamos
+        
+        if self.__enMarcha:
+            chequeo=self.__chequeo_interno()
+
+        if self.__enMarcha and chequeo:
             return 'El coche esta en marcha'
+        elif self.__enMarcha and chequeo==False:
+            return 'Algo ha ido mal en el chequeo, no podemos avanzar.'
         else:
             return 'El coche esta parado'
 
     def estado(self):
-        print('El coche tiene ', self.ruedas, 'ruedas. Un ancho de ', self.anchoChasis, 'y un largo de ', self.largoChasis)
+        print('El coche tiene ', self.__ruedas, 'ruedas. Un ancho de ', self.__anchoChasis, 'y un largo de ', self.__largoChasis)
+
+    def __chequeo_interno(self):
+        print('Realizando chequeo interno')
+        self.gasolina='ok'
+        self.aceite='ok'
+        self.puertas='cerradas'
+
+        if self.gasolina=='ok' and self.aceite=='ok' and self.puertas =='cerradas':
+            return True
+        else:
+            return False
+
 
 miCoche = Coche()
-print('El largo del coche es: ', miCoche.largoChasis)
-print('El coche tiene ', miCoche.ruedas, 'ruedas')
 print(miCoche.arrancar(True))
 miCoche.estado()
-
-print('----------------------- Segundo Objeto ---------------------------')
+print(miCoche.gasolina)
+print(miCoche.aceite)
+print(miCoche.puertas)
 
 miCoche2 = Coche()
-print('El largo del coche es: ',miCoche.largoChasis)
-print('El coche tiene ', miCoche.ruedas,' ruedas')
-print(miCoche.arrancar(False))
+print(miCoche2.arrancar(False))
 miCoche2.estado()
