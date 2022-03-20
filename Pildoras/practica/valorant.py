4f252a0143ca578ea00227a8da3cb8dfd755cdb0
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2022-03-19 21:34:51 -0500

    f

---
+++ Pildoras/practica/valorant.py
@@ -0,0 +1,35 @@
+class Armas():
+    def __init__(self,tipo,nombre,d_cabeza,d_cuerpo,d_pies,cant_balas):
+        self.tipo = tipo
+        self.nombre = nombre
+        self.d_cabeza = d_cabeza
+        self.d_cuerpo = d_cuerpo
+        self.d_pies = d_pies
+        self.cant_balas = cant_balas
+        self.balas = cant_balas
+        self.apuntando = False
+
+    def disparar(self,n):
+        self.balas -= n
+        if n <= self.balas:
+            print('Quedan',self.n,'balas.')
+        else:
+            self.recargar()
+            print('Se ha recargado, tienes',self.balas,'balas.')
+
+    def recargar(self):
+        self.balas = cant_balas
+        print('Se ha recargado, tienes',self.balas,'balas.')
+
+    def apuntar(self):
+        self.apuntando = True
+        if self.apuntando == True:
+            print('Estas apuntando')
+        else:
+            print('No estas apuntado')
+
+    def estado(self):
+        print('Tipo:',self.tipo,'\nNombre:',self.nombre,'\nDano Cabeza:',self.d_cabeza,'\nDano Cuerpo:',self.d_cuerpo,'\nDano Piernas:',self.d_pies,'\nCantidad Balas:',self.cant_balas,'\nApuntando:',self.apuntando)
+
+Vandal = Armas('Primaria','Vandal',150,39,28,25)
+Vandal.estado()
