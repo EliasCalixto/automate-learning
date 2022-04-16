86adafd26d87a64c7d1aa2c0d003479f5cd45af5
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2022-04-16 13:48:53 -0500

    fdg

--- algorithms-course/binary_search.py
+++ algorithms-course/binary_search.py
@@ -2,18 +2,14 @@ import random
 def binary_search(lista,target):
     first = 0
     last = len(lista) - 1
-    intentos = 0
     while first <= last:
         midpoint = (first+last)//2
         if lista[midpoint] == target:
-            print(lista)
             return midpoint
         elif lista[midpoint] < target:
             first = midpoint + 1
-            intentos += 1
         else:
             last = midpoint - 1
-            intentos += 1
     return None
 
 def verify(index):
