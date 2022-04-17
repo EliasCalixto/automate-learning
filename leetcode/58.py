3c82ac757d045f25b2bf600d4a9b2448b93c1231
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2022-04-16 21:06:28 -0500

    t

--- leetcode/58.py
+++ leetcode/58.py
@@ -1,6 +1,12 @@
 def lengthOfLastWord(s: str) -> int:
-    text = 
-    lenght = 0
-    print(text)
+    contador = 0
+    word = ''
+    for i in s[::-1]:
+        if i != ' ':
+            contador += 1
+            word += i
 
-lengthOfLastWord('elias')
+    finalWord = word[::-1]
+    print(f'La ultima palabra es {finalWord} y su largo es {contador}')
+
+lengthOfLastWord('   llllelias es darkesthj     ')
