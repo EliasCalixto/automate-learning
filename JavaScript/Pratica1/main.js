a8fa6db1480b2e4ff0a922346516a457e76aee20
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2022-05-05 21:03:31 -0500

    asd

---
+++ JavaScript/Pratica1/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('#y').css({
+        "background-color": 'blue',
+        "width": '50px',
+        "height": '50px',
+    });
+    $('#x').css({
+        "background-color":'red',
+        "width": '50px',
+        "height": '50px',
+    })
+});
