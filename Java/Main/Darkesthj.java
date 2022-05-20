4ec7812272f372d81e673c149e7bb3d7fd7ccaf8
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2022-05-19 23:17:53 -0500

    w

---
+++ Java/Main/Darkesthj.java
@@ -0,0 +1,13 @@
+package Main;
+
+public class Darkesthj{
+    public static void main(String[] arge){
+        int numeroEntero = 1;
+
+        double numeroDouble = 2.8;
+        char letra = 'c';
+        String frase = "Hola Mundo!";
+
+        System.out.println(numeroEntero);
+    }
+}
