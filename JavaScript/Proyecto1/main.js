1ba2ff4b60c292a05ba920036b4fda16e10e0ab1
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2022-07-02 00:35:40 -0500

    w

--- JavaScript/Proyecto1/main.js
+++ JavaScript/Proyecto1/main.js
@@ -1,29 +1,19 @@
 function onDocumentLoaded(){
-    sum = (num1,num2) => {
-        num1 = parseInt(prompt('ingrese num1:'))
-        num2 = parseInt(prompt('ingrese num2:'))
-        alert(num1+num2)
+    const root=document.getElementById('root')
+
+    const print5Divs = () =>{
+        for(let n = 0; n < 5; n++){
+            var div1 = document.createElement('div') 
+            div1.classList.add('border','rounded','bg-success','p-2','mx-auto','w-25','text-center','h3')
+            div1.textContent='asdasd'
+
+            console.log(div1)
+            root.appendChild(div1)
+        }
     }
-    res = (num1,num2) => {
-        num1 = parseInt(prompt('ingrese num1:'))
-        num2 = parseInt(prompt('ingrese num2:'))
-        alert(num1-num2)
-    }
-    mul = (num1,num2) => {
-        num1 = parseInt(prompt('ingrese num1:'))
-        num2 = parseInt(prompt('ingrese num2:'))
-        alert(num1*num2)
-    }
-    div = (num1,num2) => {
-        num1 = parseInt(prompt('ingrese num1:'))
-        num2 = parseInt(prompt('ingrese num2:'))
-        let result = (num1/num2).toFixed(2)
-        alert(result)
-    }
-    test = () =>{
-        what = (Math.random()*10000).toFixed(2)
-        alert(what)
-    }
+
+    print5Divs()
+    print5Divs()
 }
 
 document.addEventListener('DOMContentLoaded',onDocumentLoaded)
