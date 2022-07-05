0447e0ab5ab74da80e89438808aeee50e7350407
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2022-07-04 19:55:07 -0500

    w

--- JavaScript/Proyecto1/main.js
+++ JavaScript/Proyecto1/main.js
@@ -1,19 +1,38 @@
 function onDocumentLoaded(){
     const root=document.getElementById('root')
 
-    const print5Divs = () =>{
-        for(let n = 0; n < 5; n++){
-            var div1 = document.createElement('div') 
-            div1.classList.add('border','rounded','bg-success','p-2','mx-auto','w-25','text-center','h3')
-            div1.textContent='asdasd'
+    const section1 = document.createElement('section')
+        root.appendChild(section1)
 
-            console.log(div1)
-            root.appendChild(div1)
-        }
+    printSection1 = () =>{
+        section1.classList.add('border','bg-info','mx-auto','text-center','rounded','m-2')
+        const h1 = document.createElement('h1')
+        h1.classList.add('h1')
+        h1.textContent='Este es el h1'
+        section1.appendChild(h1)
+
+        const p1 = document.createElement('p')
+        p1.classList.add('h4')
+        p1.textContent='texto en el p1 asdasdasdasdasdasasdadsasadsadsasd'
+        section1.appendChild(p1)
     }
 
-    print5Divs()
-    print5Divs()
+    const createDiv2 = () =>{
+        const div2=document.createElement('div')
+        div2.classList.add('border','bg-success','mx-auto','text-center','p-4')
+        root.appendChild(div2)
+
+        const print5Divs = () =>{
+            for(let n = 0; n < 5; n++){
+                var asd = document.createElement('div') 
+                asd.classList.add('border','rounded','bg-success','p-2','mx-auto','w-25','text-center','h3')
+                asd.textContent='asdasd'
+                div2.appendChild(asd)
+            }
+        }
+        print5Divs()
+    }
+    createDiv2()
 }
 
 document.addEventListener('DOMContentLoaded',onDocumentLoaded)
--- JavaScript/Proyecto1/sum.py
+++
@@ -1,6 +0,0 @@
-def sum(n1,n2):
-    n1 = input('ingrese num1:')
-    n2 = input('ingrese num2:')
-    return n1+n2
-
-print(sum())
